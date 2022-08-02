6ae8423d6826d6ee776372a07d5c7b810258a7b3
Michele Gusmao <michele.gusmao7@gmail.com>
2022-08-02 16:46:51 -0300

    Requisito 02.

--- routers/talkerRouter.js
+++ routers/talkerRouter.js
@@ -13,4 +13,18 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const talkers = await getTalker();
+    const talker = talkers.find((talk) => talk.id === Number(id));
+    if (!talker) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    return res.status(200).json(talker);
+  } catch (err) {
+    return res.status(500).end();
+  }
+});
+
 module.exports = router;
